d7c9ed4edc97617b0ec3f06bfab9c5232b0c2366
js <js@email>
2023-12-11 06:10:14 -0800

    skeleton and para parser

--- src/FBakcupper.cpp
+++ src/FBakcupper.cpp
@@ -7,5 +7,24 @@ int main(int argc, char **argv){
     }
 
     // TODO : parser
+    doParaParser(argc, argv);
 
+    // if(备份){
+    //     if(还原) 报错；
+        
+    //     files = filter();
+
+    //     out_file = pack(files);
+
+    //     if(压缩){
+    //         out_file = docompress(out_file);
+    //     }
+    //     if(加密){
+    //         out_file = doencrypt(out_file);
+    //     }
+
+    //     out_file = metadate(out_file);
+
+    //     return;
+    // }
 }
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,8 +1,31 @@
 #include "parser.h"
+// definition
+Paras paras;
 
 void Usage(void){
-    std::cout << "Usage: FBackupper [Options] [input_file]\n\nOptions:\n";
+    std::cout << "Usage: FBackupper [Options] [Paras] ... \n\nOptions:\n";
     std::cout << "  -h, --help          Display this help message.\n";
-    std::cout << "  -b, --backup        Backup input files.\n";
-    
+    std::cout << "  -b, --backup        Do File Backup.\n";
+    std::cout << "  -r, --restore       Do File Restore.\n";
+    std::cout << "  -i, --input         Specify the path of files(or directory) to be backed up.\n";
+    std::cout << "  -o, --output        Specify where to store your backup files.\n";
+    std::cout << "  -c, --compress      Compress backup packages.\n";
+    std::cout << "  -e, --encrypt       Encrypt backup packages, must be used with -p!\n";
+    std::cout << "  -p, --password      Specify the compression password.\n";
+    std::cout << "\nOptions to filter backup files:\n";
+    std::cout << "      --name exp      Select files using regular expressions and their names, like \'*.o\' .\n";
+    std::cout << "      --size t1 t2    Select files within a specified size range, like \'1000 2000\'(Bytes).\n";
+    std::cout << "      --time t1 t2    Select files within a specified creation time range, like \'2023-12-11 20:31:12 2023-12-12 20:30:00\'.\n";
+}
+
+void doParaParser(int argc, char **argv){
+    CLI::App app{"FBackupper"};
+
+    app.add_option("-b,--backup", paras.backup, "Backup");
+    app.add_option("-r,--restore", paras.restore, "res");
+
+    CLI11_PARSE(app, argc, argv);
+
+    // TODO: check
+    return;
 }
